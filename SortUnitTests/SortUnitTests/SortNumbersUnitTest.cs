53d50fe38fb0b59a887332f3efab0c39d4511998
Rafał Major <rmaj@dgs.com>
2018-11-04 21:46:59 +0100

    Quick sort added

--- SortUnitTests/SortUnitTests/SortNumbersUnitTest.cs
+++ SortUnitTests/SortUnitTests/SortNumbersUnitTest.cs
@@ -5,9 +5,9 @@ namespace SortUnitTests
     [TestClass]
     public class SortNumbersUnitTest
     {
-        private readonly int[] randomItems = new[] { 16, 4, 19, 5, 15, 3, 18, 9, 11, 7, 20, 14, 1, 12, 8, 10, 2, 6, 13, 17 };
+        private readonly int[] randomItems = { 16, 4, 19, 5, 15, 3, 18, 9, 11, 7, 20, 14, 1, 12, 8, 10, 2, 6, 13, 17 };
 
-        private readonly int[] sortedItems = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        private readonly int[] sortedItems = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
         [TestMethod]
         public void BubbleSortTest()
@@ -23,5 +23,13 @@ namespace SortUnitTests
             var sorted = IntertSort.Sort(this.randomItems);
             CollectionAssert.AreEqual(sorted, sorted);
         }
+
+        [TestMethod]
+        public void QuickSortTest()
+        {
+            var sorted = QuickSort.Sort(this.randomItems);
+            CollectionAssert.AreEqual(sorted, sorted);
+        }
+
     }
 }
