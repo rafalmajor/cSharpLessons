abc93ca4fb9ed20099f442a95f1fb002b91b2716
Rafał Major <rmaj@dgs.com>
2018-11-04 18:41:48 +0100

    Add Bubble Sort

---
+++ SortUnitTests/SortUnitTests/SortNumbersUnitTest.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortUnitTests
+{
+    [TestClass]
+    public class SortNumbersUnitTest
+    {
+        private readonly int[] randomItems = new []{ 16, 4, 19, 5, 15, 3, 18, 9, 11, 7, 20, 14, 1, 12, 8, 10, 2, 6, 13, 17 };
+        private readonly int[] sortedItems = new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            var sorted = BubbleSort.Sort(this.randomItems);
+
+            CollectionAssert.AreEqual(sorted, this.sortedItems);
+        }
+
+    }
+}
