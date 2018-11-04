995ccd744f75b97ac8b6d27ca7a67712d44daba2
Rafał Major <rmaj@dgs.com>
2018-11-04 20:30:04 +0100

    Insert sort added

--- SortUnitTests/SortUnitTests/SortNumbersUnitTest.cs
+++ SortUnitTests/SortUnitTests/SortNumbersUnitTest.cs
@@ -1,6 +1,3 @@
-using System.Linq;
-using System.Runtime.CompilerServices;
-
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SortUnitTests
@@ -8,8 +5,9 @@ namespace SortUnitTests
     [TestClass]
     public class SortNumbersUnitTest
     {
-        private readonly int[] randomItems = new []{ 16, 4, 19, 5, 15, 3, 18, 9, 11, 7, 20, 14, 1, 12, 8, 10, 2, 6, 13, 17 };
-        private readonly int[] sortedItems = new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        private readonly int[] randomItems = new[] { 16, 4, 19, 5, 15, 3, 18, 9, 11, 7, 20, 14, 1, 12, 8, 10, 2, 6, 13, 17 };
+
+        private readonly int[] sortedItems = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
         [TestMethod]
         public void BubbleSortTest()
@@ -19,5 +17,11 @@ namespace SortUnitTests
             CollectionAssert.AreEqual(sorted, this.sortedItems);
         }
 
+        [TestMethod]
+        public void InsertSortTest()
+        {
+            var sorted = IntertSort.Sort(this.randomItems);
+            CollectionAssert.AreEqual(sorted, sorted);
+        }
     }
 }
