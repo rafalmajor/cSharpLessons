455f1cfd05b6bdbd84f421af8b0836b68daba279
Rafał Major <rmaj@dgs.com>
2018-11-06 00:33:50 +0100

    start event unit test

---
+++ SortUnitTests/EventsSortUnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventsSortUnitTests
+{
+    [TestClass]
+    public class EventSortUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+}
