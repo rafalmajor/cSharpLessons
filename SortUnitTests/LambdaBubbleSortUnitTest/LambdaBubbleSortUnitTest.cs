bc4b716ec5fb7e99c4de98a35a0414f68884d87b
Rafał Major <rmaj@dgs.com>
2018-11-06 08:41:48 +0100

    lamda project added

---
+++ SortUnitTests/LambdaBubbleSortUnitTest/LambdaBubbleSortUnitTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LambdaBubbleSortUnitTest
+{
+    [TestClass]
+    public class LambdaBubbleSortUnitTest
+    {
+        [TestMethod]
+        public void LambdaBubbleSortTest()
+        {
+
+        }
+    }
+}
